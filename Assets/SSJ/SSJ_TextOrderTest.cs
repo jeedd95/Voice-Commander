b720a60469f5a9193488b03b48b91ab95521da63
shinsuji <92511450+shinsuji@users.noreply.github.com>
2021-11-12 17:19:57 +0900

    222

--- Assets/SSJ/SSJ_TextOrderTest.cs
+++ Assets/SSJ/SSJ_TextOrderTest.cs
@@ -8,13 +8,46 @@ public class SSJ_TextOrderTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        string order = "[누구]<10,20>(으로가)";
 
+        string who = "";
+        string where = "";
+        string command = "";
+
+        // 10,20으로가
+        int head = order.IndexOf("[", 0);
+        int tail = order.IndexOf("]", head);
+        who = order.Substring(head + 1, tail - head - 1);
+
+        head = order.IndexOf("<", tail);
+        tail = order.IndexOf(">", head);
+        where = order.Substring(head + 1, tail - head - 1);
+
+        head = order.IndexOf("(", tail);
+        tail = order.IndexOf(")", head);
+        command = order.Substring(head + 1, tail - head - 1);
+
+        print(who);
+        print(where);
+        print(command);
+
+
+        string[] pos = where.Split(',');
+        float x = float.Parse(pos[0]);
+        float y = float.Parse(pos[1]);
+        print(x + ", "+ y);
+
+
+        inputFieldOrder.Select();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.Return) && false == string.IsNullOrEmpty(inputFieldOrder.text))
+        {
+            OnClickOrder();
+        }
     }
     public JHW_UnitFactory unitCreat;
     public SSJ_PlayerMove playerMove;
@@ -35,6 +68,15 @@ public class SSJ_TextOrderTest : MonoBehaviour
         {
             unitCreat.CreateUnit();
         }
+        order = "10,20으로가";
+        if (order.Contains("으로가"))
+        {
+            // 10,20으로가
+            int pos = order.IndexOf("으로가");
+        }
+
+        inputFieldOrder.text = "";
+        inputFieldOrder.Select();
     }
 
 }

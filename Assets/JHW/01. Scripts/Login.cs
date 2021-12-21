69df401f15b7d34287c5e2790230ba76866d1977
jeedd95 <hyungwoo10@naver.com>
2021-12-21 23:17:02 +0900

    .

--- Assets/JHW/01. Scripts/Login.cs
+++ Assets/JHW/01. Scripts/Login.cs
@@ -9,16 +9,29 @@ public class Login : MonoBehaviour
 {
     public static Login instance; //싱글톤
 
+    public GameObject MessageBox;
+    public Text MessageBoxText;
+    //1번째 페이지
     public InputField id;
     public InputField password;
+    public GameObject exitBtn;
+
+    //회원가입페이지
+    public InputField id2;
+    public InputField password2;
     public InputField Nickname;
+    public GameObject SignUpWindow;
+    public GameObject MessageBox2;
+    public Text MessageBoxText2;
+    public GameObject NicknameBox;
+
 
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
-          //  DontDestroyOnLoad(gameObject);
+            //  DontDestroyOnLoad(gameObject);
         }
         else if (instance != this)
         {
@@ -30,36 +43,80 @@ public class Login : MonoBehaviour
         Backend.Initialize();
     }
 
+    public void OnClickSignUpUI()
+    {
+        SignUpWindow.SetActive(true);
+        exitBtn.SetActive(false);
+
+
+    }
     public void OnClickSignUp()
     {
-        BackendReturnObject BRO = Backend.BMember.CustomSignUp(id.text, password.text);
-        if (BRO.IsSuccess())
+        BackendReturnObject BRO = Backend.BMember.CustomSignUp(id2.text, password2.text);
+        BackendReturnObject bro2 = Backend.BMember.CreateNickname(Nickname.text);
+
+
+
+
+
+
+        if(!BRO.IsSuccess())
         {
-            Debug.Log("회원가입에 성공했습니다");
+            MessageBox2.SetActive(true);
+            MessageBoxText2.text = "중복된 ID입니다";
         }
-        else
+        if (!bro2.IsSuccess())
         {
-            Debug.Log("회원가입에 실패했습니다");
+            MessageBox2.SetActive(true);
+            MessageBoxText2.text = "닉네임 생성에 실패했습니다\n20자 이상, 닉네임 중복, 앞뒤 공백이 있는 경우 사용할 수 없습니다";
         }
+        if(BRO.IsSuccess() && bro2.IsSuccess())
+        {
+            MessageBox2.SetActive(true);
+            MessageBoxText2.text = "회원가입에 성공했습니다";
+        }
+
     }
 
     public void OnClickLogin()
     {
         BackendReturnObject bro = Backend.BMember.CustomLogin(id.text, password.text);
-        Backend.BMember.CreateNickname(Nickname.text);
+
         if (bro.IsSuccess())
         {
-            Debug.Log("로그인에 성공했습니다");
-            Invoke("ToPlayScene", 2f);
+            MessageBox.SetActive(true);
+            MessageBoxText.text = "로그인에 성공했습니다 \n 잠시 후 게임이 시작됩니다";
+
+            Invoke("OnClickToPlayScene", 20f);
+
+            //Debug.Log("로그인에 성공했습니다");
         }
         else
         {
-            Debug.Log("로그인에 실패했습니다");
+            MessageBox.SetActive(true);
+            MessageBoxText.text = "로그인에 실패했습니다\n 아이디와 비밀번호를 확인해주십시오";
         }
     }
 
-    void ToPlayScene()
+    public void OnClickToPlayScene()
     {
         SceneManager.LoadScene("JHW_TestScene+Map");
+    }
+    public void OnClickExit()
+    {
+        Application.Quit();
+    }
+    public void OnClickReturn()
+    {
+        MessageBox.SetActive(false);
+    }
+    public void OnClickReturn3()
+    {
+        MessageBox2.SetActive(false);
+    }
+    public void OnClickReturn2()
+    {
+        SignUpWindow.SetActive(false);
+        exitBtn.SetActive(true);
     }
 }
--- Assets/JHW/01. Scripts/ScoreManager.cs
+++ Assets/JHW/01. Scripts/ScoreManager.cs
@@ -25,29 +25,30 @@ public class ScoreManager : MonoBehaviour
             print("점수 불러오기 실패");
             return;
         }
-
+        
         JsonData json = bro.FlattenRows(); 
         MyText.text = "NickName : "+ json[0]["NickName"].ToString()+  " / Level : " + json[0]["Level"].ToString() + " / Score : " + json[0]["score"].ToString() + " / PlayTime : " + json[0]["playTime"].ToString(); // 내 레벨,스코어,플레이타임
 
         Param param = new Param();
         //param.Add("updatedAt", json[0]["updatedAt"].ToString());
         //param.Add("Level", JHW_GameManager.instance.playerLevel);
-        param.Add("nickname", json[0]["NickName"].ToString());
-        param.Add("score", JHW_GameManager.instance.Score);
-        param.Add("playTime", JHW_GameManager.instance.playTime);
+        param.Add("nickname", json[0]["NickName"]);
+        param.Add("score", json[0]["score"]);
+        param.Add("playTime", json[0]["playTime"]);
 
         Backend.URank.User.UpdateUserScore("85eb2800-6158-11ec-85ad-571b56ff94ac", "custom", json[0]["inDate"].ToString(), param); //전체 랭킹을 업데이트함
 
         var bro2 =  Backend.URank.User.GetMyRank("85eb2800-6158-11ec-85ad-571b56ff94ac", 5); // 내 밑위 5명 불러옴
         JsonData json2 = bro2.FlattenRows(); //json2는 내 스코어와 5+- 스코어를 담아둠
         JsonData totalCount = bro2.GetFlattenJSON(); //totoalCountjson에는 다른방식으로 파싱함
-
         string a = totalCount["totalCount"].ToString();
+
+        
 
         for (int i = 0; i < json2.Count; i++)
         {
             ScoreBoard.text += "\n " +
-                                      json2[i]["rank"] + " / " + a + "\t" +
+                                      json2[i]["rank"] + " / " + "\t" +
                                       json2[i]["gamerInDate"].ToString().Substring(0,10) + "\t" +
                                       json2[i]["nickname"].ToString() + "\t" +
                                       json2[i]["score"].ToString() + "\t" +

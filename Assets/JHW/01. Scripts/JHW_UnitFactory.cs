8cd046a2fdcea0ef6451a23f9d1623d3a13b5625
jeedd95 <hyungwoo10@naver.com>
2022-01-14 14:22:34 +0900

    .

--- Assets/JHW/01. Scripts/JHW_UnitFactory.cs
+++ Assets/JHW/01. Scripts/JHW_UnitFactory.cs
@@ -43,7 +43,8 @@ public class JHW_UnitFactory : MonoBehaviour
             CreateUnit2();
         }
 
-       if(!CoroutineFlag)
+
+        if (!CoroutineFlag)
         {
             CoroutineFlag = true;
             StartCoroutine("EnemyProduct");
@@ -57,163 +58,163 @@ public class JHW_UnitFactory : MonoBehaviour
     {
         //if (Input.anyKeyDown) //유닛에 해당하는 버튼을 누른다
         //{
-            //switch (Input.inputString)
-            // {
-            //case "q":
-            //        unitIndex = 0; // RifleMan
-            //        break;
-            //    case "w": //Scout
-            //        unitIndex = 1;
-            //        break;
-            //    case "e": //Sniper
-            //        unitIndex = 2;
-            //        break;
-            //    case "r": //Artillery
-            //        unitIndex = 3;
-            //        break;
-            //    case "t": //Armoured
-            //        unitIndex = 4;
-            //        break;
-            //    case "y": //Tank
-            //        unitIndex = 5;
-            //        break;
-            //    case "u": //Helicopter
-            //        unitIndex = 6;
-            //        break;
-            //    case "i": //HeavyWeapon
-            //        unitIndex = 7;
-            //        break;
-            //    case "o": //Raptor
-            //        unitIndex = 8;
-            //        break;
-            //    default:
-            //        unitIndex = -1;
-            //        break;
-            //}
+        //switch (Input.inputString)
+        // {
+        //case "q":
+        //        unitIndex = 0; // RifleMan
+        //        break;
+        //    case "w": //Scout
+        //        unitIndex = 1;
+        //        break;
+        //    case "e": //Sniper
+        //        unitIndex = 2;
+        //        break;
+        //    case "r": //Artillery
+        //        unitIndex = 3;
+        //        break;
+        //    case "t": //Armoured
+        //        unitIndex = 4;
+        //        break;
+        //    case "y": //Tank
+        //        unitIndex = 5;
+        //        break;
+        //    case "u": //Helicopter
+        //        unitIndex = 6;
+        //        break;
+        //    case "i": //HeavyWeapon
+        //        unitIndex = 7;
+        //        break;
+        //    case "o": //Raptor
+        //        unitIndex = 8;
+        //        break;
+        //    default:
+        //        unitIndex = -1;
+        //        break;
+        //}
 
-            //if (unitIndex == -1)
+        //if (unitIndex == -1)
+        //{
+        //    return;
+        //}
+
+        units = Units[unitIndex].GetComponent<JHW_UnitInfo>(); //각 유닛의 정보를 가져온다
+
+        //(전체) 가격 검사 , 인구 수 검사
+        if (JHW_GameManager.instance.Gold >= units.price && JHW_GameManager.instance.currentPopulation + JHW_GameManager.instance._UnitLoad[unitIndex] <= JHW_GameManager.instance.wholePopulationLimit)
+        {
+            if (JHW_GameManager.instance.CoolDownReady[unitIndex]) //쿨타임까지 준비됬을때
+            {
+                InstantiateUnit(unitIndex); //유닛을 생성한다
+                ValueChanger(unitIndex); //인구수를 올린다 + 쿨타임을 돌린다
+                CoolTimeSetter(unitIndex);
+            }
+            else //쿨타임이 준비 안됬을때
+            {
+                print("쿨타임 중입니다 : " + (System.Enum.GetName(typeof(JHW_GameManager.UnitType), unitIndex)) + " " + JHW_GameManager.instance.currentCool[unitIndex] + "초 남았습니다");
+            }
+
+            #region 안쓰는 코드
+            //if (i == 0 && JHW_GameManager.instance.CoolDownReady[0])
+            //{  
+            //         JHW_GameManager.instance.currentPopulationArray[0] += JHW_GameManager.instance._UnitLoad[0];
+            //         JHW_GameManager.instance.populationSum = false;
+            //}
+            ////else if (i == 0 && JHW_GameManager.instance.RifleManCurrentPopulation >= JHW_GameManager.instance.unitMaxCount[0])
+            ////{
+            ////    print("유닛 인구수가 부족합니다");
+            ////    return;
+            ////}
+            //else if (i == 0 && !JHW_GameManager.instance.CoolDownReady[0])
             //{
+            //    print("쿨타임 중입니다 : " + JHW_GameManager.instance.currentCool[0] + "초 남았습니다");
             //    return;
             //}
 
-            units = Units[unitIndex].GetComponent<JHW_UnitInfo>(); //각 유닛의 정보를 가져온다
 
-            //(전체) 가격 검사 , 인구 수 검사
-            if (JHW_GameManager.instance.Gold >= units.price && JHW_GameManager.instance.currentPopulation + JHW_GameManager.instance._UnitLoad[unitIndex] <= JHW_GameManager.instance.wholePopulationLimit)
-            {
-                if (JHW_GameManager.instance.CoolDownReady[unitIndex]) //쿨타임까지 준비됬을때
-                {
-                    InstantiateUnit(unitIndex); //유닛을 생성한다
-                    ValueChanger(unitIndex); //인구수를 올린다 + 쿨타임을 돌린다
-                    CoolTimeSetter(unitIndex);
-                }
-                else //쿨타임이 준비 안됬을때
-                {
-                    print("쿨타임 중입니다 : " + (System.Enum.GetName(typeof(JHW_GameManager.UnitType), unitIndex)) + " " + JHW_GameManager.instance.currentCool[unitIndex] + "초 남았습니다");
-                }
+            //if (i == 1 && JHW_GameManager.instance.ScoutCurrentPopulation < JHW_GameManager.instance.unitMaxCount[1])
+            //{
+            //    JHW_GameManager.instance.ScoutCurrentPopulation++;
+            //}
+            //else if (i == 1 && JHW_GameManager.instance.ScoutCurrentPopulation >= JHW_GameManager.instance.unitMaxCount[1])
+            //{
+            //    print("유닛 인구수가 부족합니다");
+            //    return;
+            //}
+            //if (i == 2 && JHW_GameManager.instance.SniperCurrentPopulation < JHW_GameManager.instance.unitMaxCount[2])
+            //{
+            //    JHW_GameManager.instance.SniperCurrentPopulation++;
+            //}
+            //else if (i == 2 && JHW_GameManager.instance.SniperCurrentPopulation >= JHW_GameManager.instance.unitMaxCount[2])
+            //{
+            //    print("유닛 인구수가 부족합니다");
+            //    return;
+            //}
+            //if (i == 3 && JHW_GameManager.instance.ArtilleryCurrentPopulation < JHW_GameManager.instance.unitMaxCount[3])
+            //{
+            //    JHW_GameManager.instance.ArtilleryCurrentPopulation++;
+            //}
+            //else if (i == 3 && JHW_GameManager.instance.ArtilleryCurrentPopulation >= JHW_GameManager.instance.unitMaxCount[3])
+            //{
+            //    print("유닛 인구수가 부족합니다");
+            //    return;
+            //}
+            //if (i == 4 && JHW_GameManager.instance.HeavyWeaponCurrentPopulation < JHW_GameManager.instance.unitMaxCount[4])
+            //{
+            //    JHW_GameManager.instance.HeavyWeaponCurrentPopulation++;
+            //}
+            //else if (i == 4 && JHW_GameManager.instance.HeavyWeaponCurrentPopulation >= JHW_GameManager.instance.unitMaxCount[4])
+            //{
+            //    print("유닛 인구수가 부족합니다");
+            //    return;
+            //}
+            //if (i == 5 && JHW_GameManager.instance.ArmouredCurrentPopulation < JHW_GameManager.instance.unitMaxCount[5])
+            //{
+            //    JHW_GameManager.instance.ArmouredCurrentPopulation++;
+            //}
+            //else if (i == 5 && JHW_GameManager.instance.ArmouredCurrentPopulation >= JHW_GameManager.instance.unitMaxCount[5])
+            //{
+            //    print("유닛 인구수가 부족합니다");
+            //    return;
+            //}
+            //if (i == 6 && JHW_GameManager.instance.TankCurrentPopulation < JHW_GameManager.instance.unitMaxCount[6])
+            //{
+            //    JHW_GameManager.instance.TankCurrentPopulation++;
+            //}
+            //else if (i == 6 && JHW_GameManager.instance.TankCurrentPopulation >= JHW_GameManager.instance.unitMaxCount[6])
+            //{
+            //    print("유닛 인구수가 부족합니다");
+            //    return;
+            //}
+            //if (i == 7 && JHW_GameManager.instance.HelicopterCurrentPopulation < JHW_GameManager.instance.unitMaxCount[7])
+            //{
+            //    JHW_GameManager.instance.HelicopterCurrentPopulation++;
+            //}
+            //else if (i == 7 && JHW_GameManager.instance.HelicopterCurrentPopulation >= JHW_GameManager.instance.unitMaxCount[7])
+            //{
+            //    print("유닛 인구수가 부족합니다");
+            //    return;
+            //}
+            //if (i == 8 && JHW_GameManager.instance.RaptorCurrentPopulation < JHW_GameManager.instance.unitMaxCount[8])
+            //{
+            //    JHW_GameManager.instance.RaptorCurrentPopulation++;
+            //}
+            //else if (i == 8 && JHW_GameManager.instance.RaptorCurrentPopulation >= JHW_GameManager.instance.unitMaxCount[8])
+            //{
+            //    print("유닛 인구수가 부족합니다");
+            //    return;
+            //}
+            #endregion
 
-                #region 안쓰는 코드
-                //if (i == 0 && JHW_GameManager.instance.CoolDownReady[0])
-                //{  
-                //         JHW_GameManager.instance.currentPopulationArray[0] += JHW_GameManager.instance._UnitLoad[0];
-                //         JHW_GameManager.instance.populationSum = false;
-                //}
-                ////else if (i == 0 && JHW_GameManager.instance.RifleManCurrentPopulation >= JHW_GameManager.instance.unitMaxCount[0])
-                ////{
-                ////    print("유닛 인구수가 부족합니다");
-                ////    return;
-                ////}
-                //else if (i == 0 && !JHW_GameManager.instance.CoolDownReady[0])
-                //{
-                //    print("쿨타임 중입니다 : " + JHW_GameManager.instance.currentCool[0] + "초 남았습니다");
-                //    return;
-                //}
-
-
-                //if (i == 1 && JHW_GameManager.instance.ScoutCurrentPopulation < JHW_GameManager.instance.unitMaxCount[1])
-                //{
-                //    JHW_GameManager.instance.ScoutCurrentPopulation++;
-                //}
-                //else if (i == 1 && JHW_GameManager.instance.ScoutCurrentPopulation >= JHW_GameManager.instance.unitMaxCount[1])
-                //{
-                //    print("유닛 인구수가 부족합니다");
-                //    return;
-                //}
-                //if (i == 2 && JHW_GameManager.instance.SniperCurrentPopulation < JHW_GameManager.instance.unitMaxCount[2])
-                //{
-                //    JHW_GameManager.instance.SniperCurrentPopulation++;
-                //}
-                //else if (i == 2 && JHW_GameManager.instance.SniperCurrentPopulation >= JHW_GameManager.instance.unitMaxCount[2])
-                //{
-                //    print("유닛 인구수가 부족합니다");
-                //    return;
-                //}
-                //if (i == 3 && JHW_GameManager.instance.ArtilleryCurrentPopulation < JHW_GameManager.instance.unitMaxCount[3])
-                //{
-                //    JHW_GameManager.instance.ArtilleryCurrentPopulation++;
-                //}
-                //else if (i == 3 && JHW_GameManager.instance.ArtilleryCurrentPopulation >= JHW_GameManager.instance.unitMaxCount[3])
-                //{
-                //    print("유닛 인구수가 부족합니다");
-                //    return;
-                //}
-                //if (i == 4 && JHW_GameManager.instance.HeavyWeaponCurrentPopulation < JHW_GameManager.instance.unitMaxCount[4])
-                //{
-                //    JHW_GameManager.instance.HeavyWeaponCurrentPopulation++;
-                //}
-                //else if (i == 4 && JHW_GameManager.instance.HeavyWeaponCurrentPopulation >= JHW_GameManager.instance.unitMaxCount[4])
-                //{
-                //    print("유닛 인구수가 부족합니다");
-                //    return;
-                //}
-                //if (i == 5 && JHW_GameManager.instance.ArmouredCurrentPopulation < JHW_GameManager.instance.unitMaxCount[5])
-                //{
-                //    JHW_GameManager.instance.ArmouredCurrentPopulation++;
-                //}
-                //else if (i == 5 && JHW_GameManager.instance.ArmouredCurrentPopulation >= JHW_GameManager.instance.unitMaxCount[5])
-                //{
-                //    print("유닛 인구수가 부족합니다");
-                //    return;
-                //}
-                //if (i == 6 && JHW_GameManager.instance.TankCurrentPopulation < JHW_GameManager.instance.unitMaxCount[6])
-                //{
-                //    JHW_GameManager.instance.TankCurrentPopulation++;
-                //}
-                //else if (i == 6 && JHW_GameManager.instance.TankCurrentPopulation >= JHW_GameManager.instance.unitMaxCount[6])
-                //{
-                //    print("유닛 인구수가 부족합니다");
-                //    return;
-                //}
-                //if (i == 7 && JHW_GameManager.instance.HelicopterCurrentPopulation < JHW_GameManager.instance.unitMaxCount[7])
-                //{
-                //    JHW_GameManager.instance.HelicopterCurrentPopulation++;
-                //}
-                //else if (i == 7 && JHW_GameManager.instance.HelicopterCurrentPopulation >= JHW_GameManager.instance.unitMaxCount[7])
-                //{
-                //    print("유닛 인구수가 부족합니다");
-                //    return;
-                //}
-                //if (i == 8 && JHW_GameManager.instance.RaptorCurrentPopulation < JHW_GameManager.instance.unitMaxCount[8])
-                //{
-                //    JHW_GameManager.instance.RaptorCurrentPopulation++;
-                //}
-                //else if (i == 8 && JHW_GameManager.instance.RaptorCurrentPopulation >= JHW_GameManager.instance.unitMaxCount[8])
-                //{
-                //    print("유닛 인구수가 부족합니다");
-                //    return;
-                //}
-                #endregion
-
-            }
-            else if (JHW_GameManager.instance.Gold < units.price) //가격 검사 false
-            {
-                print("돈이 부족합니다");
-            }
-            else if (JHW_GameManager.instance.currentPopulation + JHW_GameManager.instance._UnitLoad[unitIndex] > JHW_GameManager.instance.wholePopulationLimit) //인구 수 검사 false
-            {
-                print("최대 인구수가 부족합니다");
-            }
-      //  }
+        }
+        else if (JHW_GameManager.instance.Gold < units.price) //가격 검사 false
+        {
+            print("돈이 부족합니다");
+        }
+        else if (JHW_GameManager.instance.currentPopulation + JHW_GameManager.instance._UnitLoad[unitIndex] > JHW_GameManager.instance.wholePopulationLimit) //인구 수 검사 false
+        {
+            print("최대 인구수가 부족합니다");
+        }
+        //  }
     }
 
 
@@ -297,9 +298,9 @@ public class JHW_UnitFactory : MonoBehaviour
         col.gameObject.tag = SelectUnit.tag;
         col.gameObject.layer = SelectUnit.layer;
 
-        
-        
-        if(SelectUnit.GetComponent<JHW_UnitInfo>().isAirForce==true)
+
+
+        if (SelectUnit.GetComponent<JHW_UnitInfo>().isAirForce == true)
         {
             Transform mcp = MyCreatePoint_Sky[randomNum]; // 1~3번 생성포인트 중에 하나 생성
             SelectUnit.transform.position = mcp.position; // 유닛들을 생성 포인트에 놓는다
@@ -313,104 +314,98 @@ public class JHW_UnitFactory : MonoBehaviour
         myUnits.Add(SelectUnit.GetComponent<JHW_UnitManager>()); //생성하면 리스트에 넣는다
     }
 
-    //[Range(1,100)]
+
+    //[Range(1, 100)]
     //public float Chance_3;
     //[Range(1, 100)]
     //public float Chance_2;
     //[Range(1, 100)]
     //public float Chance_1;
     //[Range(1, 100)]
-    //public float Chance_0 ; //맨 처음 초기확률 70퍼센트
+    //public float Chance_0; //맨 처음 초기확률 70퍼센트
 
-    public float rate=10;
-    public float temp;
-    public int[] ProductHowMany = {1,2,3};
-    public float[] Chances = new float[4]; //chance[i] =>i가 나올 확률
-    public float second=3;
-
+    public float second = 3;
     bool CoroutineFlag;
-    //int ChanceFlag =0;
+    public float[] Chances;
+
     IEnumerator EnemyProduct() //코루틴으로 생산타이밍 3초 -> 1.5초 까지 줄어듦
     {
         yield return new WaitForSeconds(second);
-        int RandomChoice = Random.Range(1,101);
-
+        int RandomChoice = Random.Range(1, 101);
 
         //유닛을 생산하기
-        if (JHW_GameManager.instance.WholePlayTime>=0 && JHW_GameManager.instance.WholePlayTime < 300) //플레이타임 0~5분
+        if (JHW_GameManager.instance.WholePlayTime >= 0 && JHW_GameManager.instance.WholePlayTime < 300) //플레이타임 0~5분
         {
             print("0~5분");
-            RateCalc(1);
+            Chances[1] = 100 - Chances[0];
         }
-        if(JHW_GameManager.instance.WholePlayTime >= 300 && JHW_GameManager.instance.WholePlayTime < 600) //5~10분
+        if (JHW_GameManager.instance.WholePlayTime >= 300 && JHW_GameManager.instance.WholePlayTime < 600) //5~10분
         {
             print("5~10분");
-            RateCalc(1);
-            RateCalc(2);
+
+            if(Chances[0] <= 0 && Chances[1] > 0)
+            {
+                Chances[2] = 100 - Chances[1];
+            }
+            else
+            {
+                Chances[2] = (100 - Chances[0]) * 0.5f;
+                Chances[1] = Chances[2];
+            }
+
         }
         if (JHW_GameManager.instance.WholePlayTime >= 600) //10분 이후
         {
             print("10분 이후");
-            RateCalc(1);
-            RateCalc(2);
-            RateCalc(3);
+            if (Chances[0] <= 0 && Chances[1] <= 0 && Chances[2]>0)
+            {
+                Chances[3] = 100 - Chances[2];
+            }
+            if(Chances[0] <=0 &&Chances[1]>0 )
+            {
+                Chances[3] = (100 - Chances[1]) * 0.5f;
+                Chances[2] = Chances[3];
+            }
+            if(Chances[0] > 0 )
+            {
+                Chances[3] = (100 - Chances[0]) * 0.5f * 0.5f;
+                Chances[2] = Chances[3];
+                Chances[1] = (100 - Chances[0]) * 0.5f;
+            }
         }
 
         if (second > 1.5) //생산 시간을 1.5초를 하한선으로 함
         {
             second -= 0.005f;
         }
+        else
+        {
+            second = 1.5f;
+        }
 
-        if (Chances[0] > 0)
+        if(Chances[0]>0)
         {
             Chances[0] -= 0.1f;
         }
-        if(Chances[0]<=0 && Chances[1]>0)
+        else if(Chances[1]>0)
         {
             Chances[0] = 0;
             Chances[1] -= 0.1f;
         }
-
-
+        else if(Chances[2]>0)
+        {
+            Chances[0] = 0;
+            Chances[1] = 0;
+            Chances[2] -= 0.1f;
+        }
+        else if(Chances[2]<=0)
+        {
+            Chances[2] = 0;
+        }
 
         CoroutineFlag = false;
     }
 
-
-    void RateCalc(int i)
-    {
-        if (i == 1) //Chances[1]은 단순히 [0]에서 뺀거로 계산
-        {
-            Chances[i] = 100 - Chances[i-1];
-            return;
-        }
-
-        Chances[i] = Chances[i - 1] * 0.5f;
-        Chances[i-1] *= 0.5f;
-
-        //temp = Chances[i - 1] - Chances[i];
-        //Chances[i - 1] = temp;
-    }
-
-    //void SwitchChance()
-    //{
-    //    switch (ChanceFlag)
-    //    {
-    //        case 0:
-    //            Chances[0] -= 0.1f;
-    //            break;
-    //        case 1:
-    //            Chances[1] -= 0.1f;
-    //            break;
-    //        case 2:
-    //            Chances[2] -= 0.1f;
-    //            break;
-    //        default:
-    //            Chances[3] = 100;
-    //            break;
-
-    //    }
-    //}
 }
 
 
